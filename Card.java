bcdf71b6b9c8165114c6914586f208511025e62e
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-02-03 11:40:00 -0500

    yo

--- Card.java
+++ Card.java
@@ -33,11 +33,79 @@ public class Card
     {
         return rank; 
     }
+    
     public int getSuit()
     {
         return suit;
     }
+
+     /**
+     * This returns the value of the card as an integer.
+     * 
+     * For facecards the value is 10, which is different
+     * than their rank underlying value. For aces the default
+     * value is 11.
+     * 
+     * @return The value of the card as an int.
+     */
+    public int getValue()
+    {
+        int value = rank;
+        if(rank > 10)
+        {
+            value = 10;
+        }
+        
+        if(rank == ACE)
+        {
+            value = 11;
+        }
+        
+        return value;
+    }
     
+    /**
+     * This utility method converts from a rank integer to a String.
+     * 
+     * @param r	The rank.
+     * @return	String version of rank.
+     */
+    public String rankToString(int r)
+    {
+        return ranks[r];
+    }
+    
+    /**
+     * This utility method converts from a suit integer to a String.
+     * 
+     * @param s	The suit.
+     * @return	String version of suit.
+     */
+    public String suitToString(int s)
+    {
+        return suits[s];
+    }
+    
+    /**
+     * Return the String version of the suit.
+     * 
+     * @return String version of suit.
+     */
+    public String getSuitAsString()
+    {
+        return suitToString(suit);
+    }
+    
+    /**
+     * Return the String version of the rank.
+     * 
+     * @return String version of the rank.
+     */
+    public String getRankAsString()
+    {
+        return rankToString(rank);
+    }
+        
     public String toString()
     {
         //Get suit for rank
