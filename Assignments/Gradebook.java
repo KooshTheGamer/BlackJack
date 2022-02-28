4e85d9121962c2c120a937481886ed7a4d4bf3ab
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-02-28 11:27:19 -0500

    khjgf

--- Assignments/Gradebook.java
+++ Assignments/Gradebook.java
@@ -1,5 +1,39 @@
 package Assignments;
+import java.util.*;
 
-public class Gradebook {
+public class Gradebook 
+{
+
+    public static void main(String args[])
+    {
+        Scanner readLine = new Scanner(System.in);
+        HashMap<String, Double> gradebook = new HashMap<String, Double>();
     
+
+        while (true)
+        {
+            String newStudent =  readLine.nextLine();
+            System.out.println("New Student (type 'end' to end): ");
+            
+            if (newStudent == "end")
+            {
+                break;
+            }
+            else
+            {
+                Double newGrade = readLine.nextDouble();
+                System.out.println("Student Grade: ");
+                gradebook.put(newStudent, newGrade);
+
+
+                System.out.println()
+            }
+    
+        }
+        
+    }
+
+
+
+
 }
