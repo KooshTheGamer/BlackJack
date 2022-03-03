c75076bc355aa82f46b44611f358912c3af12cf6
KooshTheGamer <98964763+KooshTheGamer@users.noreply.github.com>
2022-03-03 09:55:30 -0500

    urtydf

--- Assignments/Gradebook.java
+++ Assignments/Gradebook.java
@@ -11,34 +11,46 @@ public class Gradebook
         System.out.println("How many students? ");
         int numStudents = readLine.nextInt();
         
+        double[] gradebook = new double[numStudents];
 
         for(int i = 0; i < numStudents; i++)
         {
-            System.out.println("Student" + i + "Grade: ");
+            System.out.println("Student " + (i + 1)  + " Grade: ");
+            double grade = readLine.nextDouble();
+
+            gradebook[i] = grade;
+            
         }
-        // {
-            
-        //     System.out.println("New Student (type 'end' to end): ");
-        //     String newStudent =  readLine.nextLine();
-        //     if (newStudent == "end")
-        //     {
-        //         break;
-        //     }
-        //     else
-        //     {
-        //         
-        //         Double newGrade = readLine.nextDouble();
-                
-                
-            
-                
-                
-        //     }
-    
-        // }
+
+        System.out.println("Average Grade: " + average(gradebook));
+
+
+        
         
     }
 
+    public static double average(double[] gradebook)
+    {
+        double total = 0;
+        for(int i = 0; i < gradebook.length; i++)
+        {
+            total += gradebook[i];
+        }
+
+        return total / gradebook.length;
+    }
+
+    public static int lowest(double[] gradebook)
+    {
+        double lowest = gradebook[0];
+        for(int i = 0; i < gradebook.length; i++)
+        {
+            if(gradebook[i] <= gradebook[i +1])
+            {
+                
+            }
+        }
+    }
 
 
 
